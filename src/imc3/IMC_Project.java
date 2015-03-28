5e84ff20965ea9c3fcbdf3c085153c700e6c1ad8
41326040 <41326040@099059-P31S204.macklabs.br>
2015-03-27 21:25:40 -0300

    incluído meu nome

--- src/imc3/IMC_Project.java
+++ src/imc3/IMC_Project.java
@@ -1,13 +1,13 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. 
  */
 package imc3;
 
 /**
  *
- * @author 41321863
+ * @author Kaique Pantosi D'amato
  */
 import imc3.Ex2;
 import javax.swing.JFrame;
